e9325eebf37bd0d7d6d0bbc674de033da3fe35ad
youngsik <dudtlr9308@gmail.com>
2026-06-02 12:16:18 +0900

    fix: descript 한글 에러 ASC-II -> 영어로 변경

--- terraform_files/outputs.tf
+++ terraform_files/outputs.tf
@@ -1,29 +1,29 @@
 output "vpc_id" {
-  description = "생성된 VPC ID입니다."
+  description = "ID of the created VPC."
   value       = aws_vpc.main.id
 }
 
 output "public_subnet_ids" {
-  description = "Public Subnet ID 목록입니다."
+  description = "List of Public Subnet IDs."
   value       = aws_subnet.public.*.id
 }
 
 output "private_app_subnet_ids" {
-  description = "Private App Subnet ID 목록입니다."
+  description = "List of Private App Subnet IDs."
   value       = aws_subnet.private_app.*.id
 }
 
 output "db_subnet_ids" {
-  description = "DB Subnet ID 목록입니다."
+  description = "List of DB Subnet IDs."
   value       = aws_subnet.db.*.id
 }
 
 output "alb_dns_name" {
-  description = "Application Load Balancer DNS 이름입니다."
+  description = "Application Load Balancer DNS name."
   value       = aws_lb.application_load_balancer.dns_name
 }
 
 output "alb_arn" {
-  description = "Application Load Balancer ARN입니다."
+  description = "Application Load Balancer ARN."
   value       = aws_lb.application_load_balancer.arn
 }
--- terraform_files/security_group.tf
+++ terraform_files/security_group.tf
@@ -1,17 +1,17 @@
 resource "aws_security_group" "alb" {
   name        = "${var.environment}-alb-sg"
-  description = "ALB 용 Security Group"
+  description = "ALB security group"
   vpc_id      = aws_vpc.main.id
 }
 
 resource "aws_security_group" "app" {
   name        = "${var.environment}-app-sg"
-  description = "Application 서버용 Security Group"
+  description = "Application servers security group"
   vpc_id      = aws_vpc.main.id
 }
 
 resource "aws_security_group" "db" {
   name        = "${var.environment}-db-sg"
-  description = "DB 서버용 Security Group"
+  description = "DB security group"
   vpc_id      = aws_vpc.main.id
 }
