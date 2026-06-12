28efed86664d1ab85bb4a9dd3d66a002dc14c55f
youngsik <dudtlr9308@gmail.com>
2026-06-12 14:48:30 +0900

    fix: db subnet nat 라우팅 추가

--- terraform_files/route_table.tf
+++ terraform_files/route_table.tf
@@ -49,6 +49,11 @@ resource "aws_route_table_association" "private_app" {
 resource "aws_route_table" "db" {
   vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
   tags = {
     Name        = "${local.name_prefix}-db-rt"
     Environment = local.env
