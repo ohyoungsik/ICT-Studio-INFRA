910f9f3bd38580eb8e99a60d419d50a80ba37e51
youngsik <dudtlr9308@gmail.com>
2026-06-22 00:16:32 +0900

    Keep fixed EC2 instances running

---
+++ terraform_files/ec2_instance_state.tf
@@ -0,0 +1,29 @@
+resource "aws_ec2_instance_state" "bastion" {
+  instance_id = aws_instance.bastion.id
+  state       = "running"
+}
+
+resource "aws_ec2_instance_state" "master_node" {
+  instance_id = aws_instance.master_node.id
+  state       = "running"
+}
+
+resource "aws_ec2_instance_state" "db_main" {
+  instance_id = aws_instance.db_main.id
+  state       = "running"
+}
+
+resource "aws_ec2_instance_state" "postgres_primary" {
+  instance_id = aws_instance.postgres_primary.id
+  state       = "running"
+}
+
+resource "aws_ec2_instance_state" "postgres_replica1" {
+  instance_id = aws_instance.postgres_replica1.id
+  state       = "running"
+}
+
+resource "aws_ec2_instance_state" "postgres_replica2" {
+  instance_id = aws_instance.postgres_replica2.id
+  state       = "running"
+}
